581e92261db23d76c079b332d599996f512dd182
Ryo-Bamboo <bamboo.developer@gmail.com>
2020-02-28 07:39:43 +0900

    迷路探索問題の実装完了

--- ProgrammingContestChallengeBook/2-1/shortest_goal.cpp
+++ ProgrammingContestChallengeBook/2-1/shortest_goal.cpp
@@ -1,6 +1,8 @@
-#include <iostream>;
-#include <utility>;
-#include <queue>;
+#include <iostream>
+#include <utility>
+#include <queue>
+#include <algorithm>
+
 using namespace std;
 
 char field[6][6] = {
@@ -16,10 +18,50 @@ typedef pair<int, int> P;
 int Y = sizeof(field) / sizeof(field[0]);
 int X = sizeof(field[0]) / sizeof(field[0][0]);
 
-int count = 0;
+int d[6][6];
 
-void bfs(queue<P> p)
+int INF = 1000;
+
+int dx[4] = {1, 0, -1, 0};
+int dy[4] = {0, 1, 0, -1};
+
+int bfs(queue<P> p)
 {
+    for (int i = 0; i < Y; i++)
+    {
+        for (int j = 0; j < X; j++)
+        {
+            d[i][j] = INF;
+        }
+    }
+
+    P result = p.front();
+    int y = result.first;
+    int x = result.second;
+
+    d[y][x] = 0;
+
+    while (p.size())
+    {
+        P now = p.front();
+        p.pop();
+        if (field[now.first][now.second] == 'G')
+        {
+            return d[now.first][now.second];
+        }
+
+        for (int i = 0; i < 4; i++)
+        {
+            int ny = now.first + dy[i], nx = now.second + dx[i];
+
+            if (0 <= ny && ny < Y && 0 < nx && nx < X && field[ny][nx] != '#' && d[ny][nx] == INF)
+            {
+                p.push(P(ny, nx));
+                d[ny][nx] = d[now.first][now.second] + 1;
+            }
+        }
+    }
+    return 0;
 }
 
 int main()
@@ -33,7 +75,9 @@ int main()
             if (field[y][x] == 'S')
             {
                 que.push(P(y, x));
-                bfs(que);
+                int count = bfs(que);
+                printf("%d\n", count);
+                return 0;
             }
         }
     }
