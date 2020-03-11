921063f9e14e2db09e735595ddd540ec48310712
Ryo-Bamboo <bamboo.developer@gmail.com>
2020-03-12 00:42:35 +0900

    ABC/155/D 二分探索木の実装

--- ABC/155/D_Pair.cpp
+++ ABC/155/D_Pair.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+typedef long long ll;
 #define rep(i, n) for (int i = 0; i < n; i++)
 
 int main(void)
@@ -13,6 +14,22 @@ int main(void)
 
     sort(a.begin(), a.end());
 
-    
+    ll INF = ll(1e+9) + 5;
+    long long l = -INF;
+    ll r = INF;
+
+    while (l + 1 < r)
+    {
+        ll c = (l + r) / 2;
+        bool ok = [&] {
+            return false;
+        }();
+
+        if (ok)
+            l = c;
+        else
+            r = c;
+    }
+
     return 0;
 }
