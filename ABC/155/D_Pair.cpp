72c8ce5200cb5b573e0b8182d47feb26d1021f89
Ryo-Bamboo <bamboo.developer@gmail.com>
2020-03-11 22:20:20 +0900

    ABC/155/D

---
+++ ABC/155/D_Pair.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, n) for (int i = 0; i < n; i++)
+
+int main(void)
+{
+    int N, K;
+    cin >> N >> K;
+
+    vector<long long> a;
+    rep(i, N) cin >> a[i];
+
+    sort(a.begin(), a.end());
+
+    
+    return 0;
+}
